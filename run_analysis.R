aadcef11ef4578aa08aa99bd6511556a9950b244
radmar2002 <radu_marius_florin@yahoo.com>
2014-05-13 11:18:03 +0300

    code refactored

--- run_analysis.R
+++ run_analysis.R
@@ -4,6 +4,7 @@
 
 ### Author: Marius Radu
 ### Mail: radu_marius_florin@yahoo.com
+### Coursera: Getting and Cleaning Data - Peer Assesment Project
 
 
 ### R script called run_analysis.R does the following: 
@@ -18,12 +19,18 @@
 
 ### Prepare the workspace
 
-rm(list=ls())                                   ## clean memory
+rm(list=ls())                                       ## clean memory
 git_dir <- "D:/WORK_2014/Certification_Data_Science/repos/getcleandata"
-if(!file.exists(git_dir)){dir.create(git_dir)}  ## check if forlder already exists
-setwd(git_dir)                                  ## set working directory in the git clone of the project
-list.files()                                    ## check the content of the folder
-#install.packages("reshape")                    ## we need this library for the operations in Step 5 ; reshape2 package is another option
+if(!file.exists(git_dir)){dir.create(git_dir)}      ## check if forlder already exists
+setwd(git_dir)                                      ## set working directory in the git clone of the project
+list.files()                                        ## check the content of the folder
+
+
+## we need "data.table" library for the operations in Step 5 
+## and we install if it is not yet installed
+
+if("data.table" %in% rownames(installed.packages()) == FALSE){install.packages("xtable")}
+
 
 ### Download and extract the data on the local machine
 
@@ -37,32 +44,10 @@ list.files()                                        ## check the new content of 
 
 dateDownloaded <- date(); dateDownloaded            ## [1] "Sun May 11 13:18:57 2014"  date of download will apear in README file  
 
+
 ### Inspect the content to learn more about the data within 
 
 list.files(paste(git_dir, "UCI HAR Dataset", sep="/"))
-list.files(paste(git_dir, "UCI HAR Dataset/test", sep="/"))
-list.files(paste(git_dir, "UCI HAR Dataset/train", sep="/"))
-list.files(paste(git_dir, "UCI HAR Dataset/test/Inertial Signals", sep="/"))
-list.files(paste(git_dir, "UCI HAR Dataset/train/Inertial Signals", sep="/"))
-
-## Print the names ad dimensions of the files
-## from train and respectively test data files
-
-main_dir <- paste(git_dir, "UCI HAR Dataset", sep="/")
-list_train <- list.files(main_dir, pattern = "*train.txt$",full.names=TRUE, recursive=TRUE)
-list_test <- list.files(main_dir, pattern = "*test.txt$",full.names=TRUE, recursive=TRUE)
-
-for(fl in list_train){
-        curfile <- read.table(fl, quote ="", header=FALSE, sep="")   ##  sep = "" the separator is ‘white space’, that is one or !!more spaces
-        print(fl)
-        print(dim(curfile))
-}
-
-for(fl in list_test){
-        curfile <- read.table(fl, quote ="", header=FALSE, sep="")   
-        print(fl)
-        print(dim(curfile))
-}
 
 
 ### Create instrumental variables that will be used in data processing
@@ -96,8 +81,7 @@ length(aimed_columns); aimed_columns
 ## Using colClases argument of read.table() will read in memory only the aimed variables regarding mean and std
 ## reading only columns of interest is faster (checked with system.time) and economical for memory
 col_classes <- rep("NULL", 561)   
-col_classes[aimed_columns] <- "numeric"
-col_classes    ##  This is the value for the colClasses argument               
+col_classes[aimed_columns] <- "numeric"  ##  'col_classes' is the given value for the colClasses argument               
 
 
 ### 1. Merges the training and the test sets to create one data set.
@@ -140,71 +124,34 @@ aimed_columns_names <- features_file[grep("mean\\(|Mean|std",features_file$Name)
 str(aimed_columns_names) 
 ## aimed_columns_names is factor and we need the labels as character
 aimed_columns_names <- as.character(aimed_columns_names)
-aimed_columns_names <- sub("()-","_", aimed_columns_names, fixed=TRUE)
-aimed_columns_names <- sub("()","", aimed_columns_names, fixed=TRUE)
-aimed_columns_names <- gsub("\\(","_", aimed_columns_names)
-aimed_columns_names <- gsub("\\)","", aimed_columns_names)
-aimed_columns_names <- gsub("-","_", aimed_columns_names, fixed=TRUE)
-aimed_columns_names <- sub(",","_and_", aimed_columns_names, fixed=TRUE)
-aimed_columns_names
 
-str(aimed_columns_names)
+## rename the columns with more friendly names using gsub_plus local created function
 
+from <- c("()-", "()", ",", "(", ")", "-")
+to <- c("_", "", "_and_", "_", "", "_")
+
+gsub_plus <- function(pattern, replacement, x, fixed=TRUE, ...) {    ## use 'fixed' for the readibility reasons
+        for(i in 1:length(pattern))
+        x <- gsub(pattern[i], replacement[i], x, fixed=TRUE, ...)
+        return(x)
+}
+
+aimed_columns_names <- gsub_plus(from, to, aimed_columns_names)
 names(data_step4) <- c("subject_ID", "activity", aimed_columns_names)
-
-head(data_step4)
-
 
 ## Step 5: Of those columns that reflect the mean and standard deviation of the data, 
 ## calculate the mean for each person for each activity of that column.
-
-#### Approach NO 1.
-
-data_step5 <- data_step4
-head(data_step5)
-dim(data_step5)
-
-# will use the melt and cast functions from "reshape" library
-library(reshape)
-melted_data_step5 <- melt(data_step5, id=c("subject_ID","activity")) 
-head(melted_data_step5)
-dim(melted_data_step5)
-
-cast_data_step5 <- cast(melted_data_step5, subject_ID + activity ~ variable, mean)
-head(cast_data_step5,10)
-dim(cast_data_step5)
-
-
-#### Approach NO 2.
-
-data_step5b <- data_step4
-head(data_step5b)
-dim(data_step5b)
 
 ## Using data.table package and key will make the aggregations faster than (any) other 
 ## alternative procedures e.g. aggregate, plyr:ddply, reshape2:melt and cast, classic "for" loop
 ## vignette("datatable-timings")
 
-#install.packages("data.table") 
 library(data.table)
 
-DT <- data.table(data_step5b, key=c("subject_ID", "activity"))  ## create a 2-column key
-str(DT)
-tables()
-
-cast_data_step5b <- DT[,lapply(.SD, mean(..., na.rm=TRUE)), by=c("subject_ID", "activity")]
-as.data.frame(cast_data_step5b)[1:5,1:5] == cast_data_step5[1:5,1:5]
-
-DT[,lapply(.SD, mean(..., na.rm=TRUE)), by=c("subject_ID", "activity")]
-DT[,lapply(.SD, mean), by=c("subject_ID", "activity")]   ## apply mean through columns by group
+DT <- data.table(data_step4, key=c("subject_ID", "activity"))                          ## create a 2-column key
+# str(DT); tables()                                                                    ## inspect data structures
+data_step5 <- DT[,lapply(.SD, mean(..., na.rm=TRUE)), by=c("subject_ID", "activity")]  ## apply mean through columns by group
 
 ##  Export from R the tidy data set into a tab delimited .txt file
-write.table(cast_data_step5, "tidy.txt", row.names=FALSE, col.names=TRUE, sep="\t")
-
-
-# should check this...
-# In the previous run of the course, students have generally ignored the "Inertial Signals" folder.
-# It seemed to be an acceptable choice in the case it was documented appropriately.
-# Documenting the steps you are following is an important aspect of this project.
-
+write.table(as.data.frame(data_step5), "tidy.txt", row.names=FALSE, col.names=TRUE, sep="\t")
 
